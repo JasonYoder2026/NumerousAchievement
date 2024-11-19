a9b88515b7503a2135884bd66616142a94880dca
Jason <141192101+jyod27@users.noreply.github.com>
2024-11-18 21:23:10 -0500

    Adds more unit tests to ensure it calculates the roman numeral right

--- src/test/java/edu/bsu/cs/IntToRomanNumeralTest.java
+++ src/test/java/edu/bsu/cs/IntToRomanNumeralTest.java
@@ -58,8 +58,18 @@ public class IntToRomanNumeralTest {
     }
 
     @Test
+    void testConvert87() {
+        assertEquals("LXXXVII", convert.convertToRomanNumeral(87));
+    }
+
+    @Test
     void testConvert90() {
         assertEquals("XC", convert.convertToRomanNumeral(90));
+    }
+
+    @Test
+    void testConvert95() {
+        assertEquals("XCV", convert.convertToRomanNumeral(95));
     }
 
     @Test
@@ -68,8 +78,18 @@ public class IntToRomanNumeralTest {
     }
 
     @Test
+    void testConvert286() {
+        assertEquals("CCLXXXVI", convert.convertToRomanNumeral(286));
+    }
+
+    @Test
     void testConvert400() {
         assertEquals("CD", convert.convertToRomanNumeral(400));
+    }
+
+    @Test
+    void testConvert455() {
+        assertEquals("CDLV", convert.convertToRomanNumeral(455));
     }
 
     @Test
@@ -78,13 +98,38 @@ public class IntToRomanNumeralTest {
     }
 
     @Test
+    void testConvert823() {
+        assertEquals("DCCCXXIII", convert.convertToRomanNumeral(823));
+    }
+
+    @Test
+    void testConvert824() {
+        assertEquals("DCCCXXIV", convert.convertToRomanNumeral(824));
+    }
+
+    @Test
     void testConvert900() {
         assertEquals("CM", convert.convertToRomanNumeral(900));
     }
 
     @Test
+    void testConvert948() {
+        assertEquals("CMXLVIII", convert.convertToRomanNumeral(948));
+    }
+
+    @Test
     void testConvert1000() {
         assertEquals("M", convert.convertToRomanNumeral(1000));
+    }
+
+    @Test
+    void testConvert2563() {
+        assertEquals("MMDLXIII", convert.convertToRomanNumeral(2563));
+    }
+
+    @Test
+    void testConvert3000() {
+        assertEquals("MMM", convert.convertToRomanNumeral(3000));
     }
 
     @Test
