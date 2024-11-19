45384ebd9e160150e3e8fd530d31f4e64cfc6c0c
Jason <141192101+jyod27@users.noreply.github.com>
2024-11-18 20:46:50 -0500

    Refactors code to make finding results more efficient and not a chain of if/else statements.

--- src/main/java/edu/bsu/cs/IntToRomanNumeral.java
+++ src/main/java/edu/bsu/cs/IntToRomanNumeral.java
@@ -2,23 +2,16 @@ package edu.bsu.cs;
 
 public class IntToRomanNumeral {
     public String convertToRomanNumeral(int givenNumber) {
+        int[] integers = {1, 5, 10, 50, 100, 500, 1000};
+        String[] romanNumerals = {"I", "V", "X", "L", "C", "D", "M"};
+
         if (givenNumber < 1 || givenNumber > 3000) {
             return "Not valid input";
         }
-        if (givenNumber == 1) {
-            return "I";
-        } else if (givenNumber == 5) {
-            return "V";
-        } else if (givenNumber == 10) {
-            return "X";
-        } else if (givenNumber == 50) {
-            return "L";
-        } else if (givenNumber == 100) {
-            return "C";
-        } else if (givenNumber == 500) {
-            return "D";
-        } else if (givenNumber == 1000) {
-            return "M";
+        for (int i = 0; i < integers.length; i++) {
+            if(givenNumber == integers[i]) {
+                return romanNumerals[i];
+            }
         }
         return null;
     }
